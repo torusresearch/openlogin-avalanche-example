bf10e265f050fa03d1b652aa4516a6370501f9b2
himanshuchawla009 <himanshuchawla2014@gmail.com>
2021-04-04 19:05:14 +0530

    example simplified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./containers/login";
-import Avalanche from "./containers/avalanche";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./App.scss";
 
@@ -12,9 +11,6 @@ function App() {
       <Switch>
         <Route path="/" exact>
           <Login />
-        </Route>
-        <Route path="/avalanche" exact>
-          <Avalanche />
         </Route>
       </Switch>
     </div>
